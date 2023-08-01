2d90fd5b401ef59f1f08aec76f1dfd17005f6a99
Leo16012000 <67458956+Leo16012000@users.noreply.github.com>
2023-08-01 07:29:18 +0700

    add h2 database

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,28 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public void run(String... args) throws Exception {
+        // Khi chương trình chạy
+        // Insert vào csdl một user.
+        User user = new User();
+        user.setUsername("leo");
+        user.setPassword(passwordEncoder.encode("leo"));
+        userRepository.save(user);
+        System.out.println(user);
     }
 }
--- src/main/java/org/example/WebSecurityConfig.java
+++ src/main/java/org/example/WebSecurityConfig.java
@@ -1,36 +1,48 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserService userService;
+
     @Bean
-    @Override
-    public UserDetailsService userDetailsService(){
-        //Create user in the memory
-        //Only for illustration :>>
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(
-                User.withDefaultPasswordEncoder().username("rekkei").password("123456").roles("USER").build()
-        );
-        return manager;
+    public PasswordEncoder passwordEncoder() {
+        // Password encoder, để Spring Security sử dụng mã hóa mật khẩu người dùng
+        return new BCryptPasswordEncoder();
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().antMatchers("/", "/home").permitAll() //Allow to access these 2 addresses
-                .anyRequest().authenticated() //Requests need to authenticated
-                .and().formLogin() //Allow user authenticate using form login
-                .defaultSuccessUrl("/hello")
-                .permitAll() //Tát cả đều được truy cập vào địa chỉ này
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.userDetailsService(userService) // Cung cáp userservice cho spring security
+                .passwordEncoder(passwordEncoder()); // cung cấp password encoder
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll() // Cho phép tất cả mọi người truy cập vào 2 địa chỉ này
+                .anyRequest().authenticated() // Tất cả các request khác đều cần phải xác thực mới được truy cập
                 .and()
-                .logout()
+                .formLogin() // Cho phép người dùng xác thực bằng form login
+                .defaultSuccessUrl("/hello")
+                .permitAll() // Tất cả đều được truy cập vào địa chỉ này
+                .and()
+                .logout() // Cho phép logout
                 .permitAll();
     }
 }
